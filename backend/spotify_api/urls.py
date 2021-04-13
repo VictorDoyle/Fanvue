6ce8ba896e8f4129850d1095fc29bee716035db0
victordoyle <victor-doyle@outlook.com>
2021-04-13 19:59:03 -0400

    added project urls for later jwt module setup and api calls for react

--- backend/spotify_api/urls.py
+++ backend/spotify_api/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #api calls urls
+    path('api/', include('main_app.urls')),
     #All auth path
     path('accounts/', include('allauth.urls')),
+    #JWT rest-framework 
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
 ]
