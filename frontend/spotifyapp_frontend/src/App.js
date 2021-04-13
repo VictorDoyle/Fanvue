d9f0de6c09ccc216316c44269f0b96d2c9b870e5
victordoyle <victor-doyle@outlook.com>
2021-04-13 19:48:54 -0400

    base setup with npm packages + django backend setup

---
+++ frontend/spotifyapp_frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import routes from './config/routes'
+function App() {
+  return (
+    <>
+    {/* pages included in routes */}
+    {routes}
+
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/spotifyapp_frontend/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function LandingPage() {
+    return(
+        <>
+        Landing Page
+        </>
+    )
+}
+
+export default LandingPage
